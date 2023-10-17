56b5ef17e271f96a5fdcaea2a5a7e8d04305d135
Max Kainov <max.kainov@clickhouse.com>
2023-10-17 16:13:04 +0000

    Build digester

---
+++ tests/ci/artifact-digester.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../../
+
+# Source files hash:
+SOURCE_DIGEST=$(find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | xargs md5sum | awk '{ print $1 }' | sort | md5sum | awk '{ print $1 }')
+
+
+find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | head -n50
+find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | wc -l
+echo TAILS
+find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | tail -n50
+echo 'Hashes:'
+find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | xargs md5sum | awk '{ print $1 }' | sort | head -n50
+echo TAILS
+find $DIR/src $DIR/contrib/**-cmake -type f  | grep -vE '*.md$' | xargs md5sum | awk '{ print $1 }' | sort | tail -n50
+echo "SOURCE_DIGEST=${SOURCE_DIGEST}"
+
+# Modules hash:
+#   git submodule status cmd works regardeles wether modules are cloned or not, drop possible +/- sign as it shows the status (cloned/ not cloned/ changed files) and may vary
+MODULES_DIGEST=$(cd $DIR; git submodule status | awk '{ print $1 }' | sed 's/^[ +-]//' | md5sum | awk '{ print $1 }')
+echo "MODULES_DIGEST=${MODULES_DIGEST}"
+
+# Docker builder image hash
+DOCKER_BUILDER_DIGEST=$(find $DIR/docker/packager/ $DIR/docker/test/util $DIR/docker/test/base -type f | grep -vE '*.md$' | xargs md5sum | awk '{ print $1 }' | sort | md5sum | awk '{ print $1 }')
+echo "DOCKER_BUILDER_DIGEST=${DOCKER_BUILDER_DIGEST}"
+
+BUILD_DIGEST=$(echo $SOURCE_DIGEST-$MODULES_DIGEST-$DOCKER_DIGEST | md5sum | awk '{ print $1 }')
+echo "BUILD_DIGEST=${BUILD_DIGEST}"
+export BUILD_DIGEST=$BUILD_DIGEST
